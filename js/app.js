2eae7e0599c7b8067d413689c63d8a3f8eefe43d
Juan Gonon <juanfegonon27@gmail.com>
2024-03-05 11:46:03 -0600

    custom error

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ import { valida } from "./script.js";
 const inputs = document.querySelectorAll("input");
 
 inputs.forEach((element)=>{
-    console.log(element)
     element.addEventListener('blur', (e)=>{
         valida(e.target)
     })
--- js/script.js
+++ js/script.js
@@ -7,7 +7,32 @@ export function valida(input){
         validadores[tipoInput](input)
     }
 
+    console.log(input.parentElement)
 
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+    }
+
+
+}
+
+const mensajeError = {
+    name: {
+        valueMissing : "Este campo no puede esta vacío"
+    },
+    email:{
+        typeMismatch : "El correo no es válido"
+    },
+    password:{
+        valueMissing : "Este campo no puede esta vacío",
+        patternMismatch : "Al menos 6 caracteres, máximo 12"
+    },
+    nacimiento:{
+        valueMissing : "Este campo no puede esta vacío",
+        customError: "Debes tener al menos 18 años de edad"
+    }
 }
 
 const validadores = {
